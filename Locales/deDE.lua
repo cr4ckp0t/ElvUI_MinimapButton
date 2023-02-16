902f2b25a6906cf5031bff25bff5ad5d6e8bd68c
Adam Koch <me@adamko.ch>
2023-02-16 12:50:05 -0500

    Fix AceLocale errors.

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -1,12 +1,8 @@
 -------------------------------------------------------------------------------
 -- ElvUI_MinimapButton By Crackpot (US, Illidan)
 -------------------------------------------------------------------------------
-local debug = false
---@debug@
-debug = true
---@end-debug@
-local E, _, _, _, _ = unpack(ElvUI)
-local L = E.Libs.ACL:NewLocale("ElvUI_MinimapButton", "deDE", true, debug)
+local L = ElvUI[1].Libs.ACL:NewLocale("ElvUI_MinimapButton", "deDE", false)
 if not L then return end
 
 --@localization(locale="deDE", format="lua_additive_table", same-key-is-true=true, escape-non-ascii=true, handle-unlocalized="english")@
+
